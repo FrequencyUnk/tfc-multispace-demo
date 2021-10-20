42b11caa161ab93da8b327138d4082d2bfacbec9
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-10-19 20:04:40 -0500

    Update main.tf
    Changing workspace name attribute to have a generic multispace naming scheme

--- main.tf
+++ main.tf
@@ -11,29 +11,29 @@ resource "multispace_run" "root" {
   # Use string workspace names here and not data sources so that
   # you can define the multispace runs before the workspace even exists.
   organization = "bluebelt-dev"
-  workspace    = "tfc"
+  workspace    = "multispace-home"
 }
 
 resource "multispace_run" "physical" {
   organization = "bluebelt-dev"
-  workspace    = "k8s-physical"
+  workspace    = "multispace-physical"
   depends_on   = [multispace_run.root]
 }
 
 resource "multispace_run" "core" {
   organization = "bluebelt-dev"
-  workspace    = "k8s-core"
+  workspace    = "multispace-core"
   depends_on   = [multispace_run.physical]
 }
 
 resource "multispace_run" "dns" {
   organization = "bluebelt-dev"
-  workspace    = "dns"
+  workspace    = "multispace-dns"
   depends_on   = [multispace_run.root]
 }
 
 resource "multispace_run" "ingress" {
   organization = "bluebelt-dev"
-  workspace    = "ingress"
+  workspace    = "multispace-ingress"
   depends_on   = [multispace_run.core, multispace_run.dns]
 }
